9445cd51334f602ed7eda96b87c8c1a5e9047309
doroddong <dongpang1014@ajou.ac.kr>
2019-08-09 20:32:02 +0900

    Frist Commit

---
+++ main.js
@@ -0,0 +1,20 @@
+function addNewItem(list,itemText){
+    var listItem = document.createElement('li');
+    
+    listItem.innerText = itemText;
+    list.appendChild(listItem);
+}
+
+var inputText = document.getElementById('inputText');
+inputText.focus();
+
+inputText.onkeyup = function(event) {
+    //Event.which(13) === Enter Key!!
+    if(event.which === 13){
+        var itemText = inputText.value;
+        if(!itemText || itemText ==="" || itemText ===" ") return false;        //blank 방지
+        addNewItem(document.getElementById('todolist'),itemText);
+        inputText.focus();
+        inputText.select();
+    }
+};
